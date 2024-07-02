d43f48730d913cb7c17c50c061c59e5ce13f000b
Felipe <felipefragam@gmail.com>
2024-07-02 01:33:51 -0300

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+      <div className="min-h-screen">
+      <Component {...pageProps} />
+      </div>
+      <ThemeToggle className="absolute top-6 right-6" />
+    </NextThemesProvider>
+  )
 }
